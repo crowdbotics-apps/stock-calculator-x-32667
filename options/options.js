67c0e4b25971243522b10434a6b72da4aed6a168
Crowdbotics <team@crowdbotics.com>
2022-01-03 08:58:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "stock_calculator_x_32667",
+  url: "https://stock_calculator_x_32667.botics.co",
+  api: "https://stock_calculator_x_32667.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
